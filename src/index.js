bed88805e79a12aea7cf4d564bd2c62f5d83ac50
anuva04 <57056078+anuva04@users.noreply.github.com>
2021-05-17 15:00:12 +0530

    reducer composition with objects

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {createStore} from 'redux';
+// var redux = require('react-redux');
+
 
 // todo reducer for carrying out an action for a single todo
 const todo = (state, action) => {
@@ -37,6 +40,27 @@ const todos = (state = [], action) => {
       return state;
   }
 };
+
+// reducer for changing visibility of todos
+// default state is SHOW_ALL
+const visibilityFilter = (state='SHOW_ALL', action) => {
+  switch(action.type) {
+    case 'SET_VISIBILITY_FILTER':
+      return action.filter;
+    default:
+      return state;
+  }
+};
+
+const todoApp = (state = {}, action) => {
+  return {
+    todos: todos(state.todos, action),
+    visibilityFilter : visibilityFilter(state.visibilityFilter, action)
+  };
+};
+
+const store = createStore(todoApp);
+
 class TodoApp extends React.Component {
   render() {
     return (

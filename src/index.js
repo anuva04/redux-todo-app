5d5b7bf2dc4b773a30f94364e3446f59c1d96875
anuva04 <57056078+anuva04@users.noreply.github.com>
2021-05-17 14:31:55 +0530

    ADD_TODO and TOGGLE_TODO actions

--- src/index.js
+++ src/index.js
@@ -2,6 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const todos = (state = [], action) => {
+  switch (action.type){
+    case 'ADD_TODO':
+      return [
+        ...state,
+        {id: action.id, text: action.text, comleted: false}
+      ];
+    case 'TOGGLE_TODO':
+      return state.map(todo => {
+        if(todo.id !== action.id) return todo;
+        return {
+          ...todo,
+          completed: !todo.completed
+        };
+      });
+    default:
+      return state;
+  }
+};
 class TodoApp extends React.Component {
   render() {
     return (
